1499509f2c7f3ba8efe38a903b16296ebc0555b1
Liam <liam.choi@ivendi.com>
2023-05-02 16:31:26 +0100

    This is working

--- NancyNCSRF/Startup.cs
+++ NancyNCSRF/Startup.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Cryptography;
 using Nancy.Owin;
+using Nancy.Security;
+using Nancy.TinyIoc;
 
 namespace NancyNCSRF
 {
@@ -8,7 +13,32 @@ namespace NancyNCSRF
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.UseOwin(x => x.UseNancy());
+            var options = new NancyOptions();
+            options.Bootstrapper = new ApiBootstrapper();
+            app.UseOwin(x => x.UseNancy(options));
+        }
+    }
+
+    public class ApiBootstrapper : DefaultNancyBootstrapper
+    {
+        public ApiBootstrapper()
+        {
+            Console.WriteLine("running");
+
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            var csfrCryptographyConfiguration = new CryptographyConfiguration(
+                new AesEncryptionProvider(new PassphraseKeyGenerator("blahblahblah", new byte[] { 8, 2, 10, 4, 68, 120, 7, 14 })),
+                new DefaultHmacProvider(new PassphraseKeyGenerator("blahblahblah", new byte[] { 8, 2, 10, 4, 68, 120, 7, 14 })));
+
+            container.Register<CryptographyConfiguration>(csfrCryptographyConfiguration);
+
+            var csrfAppStart = new CsrfApplicationStartup(csfrCryptographyConfiguration, new DefaultCsrfTokenValidator(csfrCryptographyConfiguration));
+
+            csrfAppStart.Initialize(pipelines);
+
         }
     }
 }
